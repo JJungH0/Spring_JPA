fa486856d7952990d019ab306b4db647250a4b6f
JJung0 <junghwan__@naver.com>
2025-05-28 15:08:12 +0900

    주석추가

--- src/main/java/com/nhnacademy/springbootjpa/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/MemberRepository.java
@@ -10,6 +10,9 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // TODO #1: @EntityGraph를 사용하여 N+1 문제를 해결하세요.
+    // 쿼리dsl과 비교하자면
+    // 그래프는 -> 런타임시에 오류가 발생할수있고
+    // 쿼리dsl -> 컴파일시에 오류를 감지할수있어서 배포 시 에러가 적다.
     @EntityGraph(attributePaths = "locker")
     List<Member> findAllBy();
 }
