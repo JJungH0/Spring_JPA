2fa31918f3bcf1d8bd0b733caf8485989ae7c88c
JJung0 <junghwan__@naver.com>
2025-05-28 15:05:52 +0900

    문제해결!

--- src/main/java/com/nhnacademy/springbootjpa/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.springbootjpa.repository;
 
 import com.nhnacademy.springbootjpa.entity.Member;
+import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +10,6 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // TODO #1: @EntityGraph를 사용하여 N+1 문제를 해결하세요.
+    @EntityGraph(attributePaths = "locker")
     List<Member> findAllBy();
 }
