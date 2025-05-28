4b851f7caaed92b57f2253c8e080fbe9384b403d
JJung0 <junghwan__@naver.com>
2025-05-28 10:42:13 +0900

    실습완료

--- src/main/java/com/nhnacademy/springbootjpa/controller/GetUserController.java
+++ src/main/java/com/nhnacademy/springbootjpa/controller/GetUserController.java
@@ -2,6 +2,7 @@ package com.nhnacademy.springbootjpa.controller;
 
 import com.nhnacademy.springbootjpa.entity.User;
 import com.nhnacademy.springbootjpa.error.ResourceNotFoundException;
+import com.nhnacademy.springbootjpa.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +13,19 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @RestController
 public class GetUserController {
+//    private UserRepository repository;
 
+    // DomainClassConverter를 이용한 API
     // TODO #1: DomainClassConverter를 이용해서 path variable로 Entity를 받음
     @GetMapping("/users/{id}")
     public User getById(@PathVariable("id") Optional<User> optionalUser) {
         return optionalUser.orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
+    // 기존에 해야하는 API
+//    @GetMapping("/users/{id}")
+//    public User getById2(@PathVariable("id") String id) {
+//        return repository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
+//    }
 }
