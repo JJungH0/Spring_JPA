246ca21016cb794355ebef8c7ff13f468edb18f3
JJung0 <junghwan__@naver.com>
2025-05-28 14:27:10 +0900

    실습완료

--- src/main/java/com/nhnacademy/springbootjpa/repository/OrderItemRepository.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/OrderItemRepository.java
@@ -4,5 +4,5 @@ import com.nhnacademy.springbootjpa.entity.OrderItem;
 import com.nhnacademy.springbootjpa.entity.OrderItemPk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk>{
 }
--- src/main/java/com/nhnacademy/springbootjpa/repository/OrderQuerydslRepositoryImpl.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/OrderQuerydslRepositoryImpl.java
@@ -1,20 +1,20 @@
 package com.nhnacademy.springbootjpa.repository;
 
-import com.nhnacademy.springbootjpa.entity.Item;
+import com.nhnacademy.springbootjpa.entity.*;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
 
 // TODO #3: 실습 - querydsl를 이용해 custom repository를 구현하려면 상속받아야 하는 추상 클래스를 작성하세요.
-public class OrderQuerydslRepositoryImpl extends Object/*???*/ implements OrderQuerydslRepository {
+public class OrderQuerydslRepositoryImpl extends QuerydslRepositorySupport implements OrderQuerydslRepository {
     public OrderQuerydslRepositoryImpl() {
+        super(Order.class);
         // TODO #4: 실습 - 적절한 생성자 호출 구문을 쓰세요.
     }
 
     @Override
     public List<Item> findAllByHavingOrderItemQuantityGreaterThan(int quantity) {
-        return null;
         // TODO : #5 4번 항목까지 모두 완료하였으면 위의 return null 구문을 삭제하고 아래 주석을 해제하세요.
-        /*
         QOrder order = QOrder.order;
         QOrderItem orderItem = QOrderItem.orderItem;
         QItem item = QItem.item;
@@ -26,6 +26,5 @@ public class OrderQuerydslRepositoryImpl extends Object/*???*/ implements OrderQ
                 .select(item)
                 .distinct()
                 .fetch();
-        */
     }
 }
